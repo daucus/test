6f034d0ccab4fe9bc72bdc3fcec25747ac785045
daucus <xh1468822083@163.com>
2018-06-28 14:13:12 +0800

    对Test增加输出默认字符集。

--- Test.java
+++ Test.java
@@ -1,5 +1,8 @@
+import java.nio.charset.*;
+
 public class Test {
   public static void main(String[] args) {
     System.out.println("Hello world");
+    System.out.println(Charset.defaultCharset());
   }
 }
